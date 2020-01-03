0b2cded794bc5a8878a2d816504d471c3166313a
Lian <47261500+KnightLian@users.noreply.github.com>
2020-01-03 17:15:30 -0500

    Update 21_Merge_Two_Sorted_Lists.py

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -1,7 +1,13 @@
-#Merge two sorted linked lists and return it as a new list. 
-#The new list should be made by splicing together the nodes of the first two lists.
-#Input: 1->2->4, 1->3->4
-#Output: 1->1->2->3->4->4
+'''
+Merge two sorted linked lists and return it as a new list. 
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+'''
+
 
 # Definition for singly-linked list.
 # class ListNode:
